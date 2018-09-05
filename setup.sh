62e2d921e015001e18d6a93589444ae85c8b1a7d
njoseph1 <40042905+njoseph1@users.noreply.github.com>
2018-09-05 15:13:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 # also set any other ROS environment variables here.
 
 source ../../devel/setup.bash
-export ROS_IP=192.168.42.1
-#export ROS_HOSTNAME=<enterhostnamehere>
+#export ROS_IP=192.168.42.1
+export ROS_HOSTNAME=ubiquityrobot
 #export ROS_MASTER_URI=http://<enterhostnamehere>:11311
 export PS1="\[\033[00;33m\][walkerbot - ${ROS_MASTER_URI}]\[\033[00m\] $PS1"
