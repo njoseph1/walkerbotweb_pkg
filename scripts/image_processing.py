76e4d7461b525fa35f59e475a4f4d9e7da9a93a4
njoseph1 <40042905+njoseph1@users.noreply.github.com>
2018-09-06 15:50:01 -0400

    Update image_processing.py

--- scripts/image_processing.py
+++ scripts/image_processing.py
@@ -9,20 +9,21 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
 
-class ImageReshaper (picamera.array.PiMotionAnalysis) :
+class ImageReshaper (picamera.array.PiMotionAnalysis):
     def setup(self):
-        self.image = None 
+        self.image = None
 
     def write(self, data):
-        self.image = np.reshape(np.fromstring(data, dtype=np.uint8), (240, 320, 3))
-    
+        self.image = np.reshape(np.fromstring(
+            data, dtype=np.uint8), (240, 320, 3))
+
 
 def main():
 
-    
     rospy.init_node('camera')
 
-    image_pub = rospy.Publisher("/walkerbotweb/image_raw", Image, queue_size=1, tcp_nodelay=False)
+    image_pub = rospy.Publisher(
+        "/walkerbotweb/image_raw", Image, queue_size=1, tcp_nodelay=False)
 
     print "Vision Started"
 
@@ -33,18 +34,18 @@ def main():
             camera.resolution = (320, 240)
             with ImageReshaper(camera) as image_reshaper:
 
-                    image_reshaper.setup()
-                    camera.start_recording(image_reshaper, format='bgr')
-                    # nonblocking wait
-                    while not rospy.is_shutdown():
-                        camera.wait_recording(1/100.0)
+                image_reshaper.setup()
+                camera.start_recording(image_reshaper, format='bgr')
+                # nonblocking wait
+                while not rospy.is_shutdown():
+                    camera.wait_recording(1 / 100.0)
 
-                        if image_reshaper.previous_image is not None:
-                            image_message = bridge.cv2_to_imgmsg(image_reshaper.previous_image, encoding="bgr8")
-                            image_pub.publish(image_message)
+                    if image_reshaper.previous_image is not None:
+                        image_message = bridge.cv2_to_imgmsg(
+                            image_reshaper.previous_image, encoding="bgr8")
+                        image_pub.publish(image_message)
 
             camera.stop_recording()  # stop recording both the flow
-
 
         print "Shutdown Received"
         sys.exit()
